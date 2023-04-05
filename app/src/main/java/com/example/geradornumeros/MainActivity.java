538ddaa82eb5e8f2fe97db15abbc10d69604771f
Joice-Opolis <joice.opolis@gmail.com>
2023-04-05 20:00:29 -0300

    iniciado sorteio de numeros, não deu certo ainda

--- app/src/main/java/com/example/geradornumeros/MainActivity.java
+++ app/src/main/java/com/example/geradornumeros/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.geradornumeros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText idQtdNum;
+    Button bGerarNum;
+    TextView idNumGerados;
+
+    int vetorNumeros[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idQtdNum=findViewById(R.id.idQtdNum);
+        bGerarNum=findViewById(R.id.bGerarNum);
+        idNumGerados=findViewById(R.id.idNumGerados);
+
+        bGerarNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String idPegaQtdNum = idQtdNum.getText().toString();
+                int intIdPegaQtdNum = Integer.parseInt(idPegaQtdNum);
+
+                vetorNumeros = new int [intIdPegaQtdNum];
+
+
+                for (int i: vetorNumeros){
+                    int sorteiaNumeros = (int) (Math.random()*intIdPegaQtdNum);
+                    vetorNumeros.getClass(sorteiaNumeros);
+                }
+            }
+        });
+
     }
 }
